6220878c889213f5beb8b7f74d7cb35bbc6cf5f9
Felicity Graham <Felicity.rose.graham@outlook.com>
2023-12-15 12:48:52 -0800

    bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -1,22 +1,17 @@
-def bubbleSort(arr):
-    n = len(arr)
-     
-    for i in range(n):
-        swapped = False
- 
-        for j in range(0, n-i-1):
- 
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                swapped = True
-        if (swapped == False):
-            break
+from typing import List
 
-if __name__ == "__main__":
-    arr = [64, 34, 25, 12, 22, 11, 90]
- 
-    bubbleSort(arr)
- 
-    print("Sorted array:")
-    for i in range(len(arr)):
-        print("%d" % arr[i], end=" ")
+def swap(arr, right_pos, left_pos):
+  temp = arr[left_pos]
+  arr[left_pos] = arr[right_pos]
+  arr[right_pos] = temp
+
+
+def bubble_sort(arr):
+  for itm in arr:
+    for idx in range(len(arr) - 1):
+      if arr[idx] < arr[idx + 1]:
+        swap(arr, idx, idx + 1)
+
+myarray = [5, 7, 4, 3, 8, 10, 12, 6, 0, 41, 2]
+bubble_sort(myarray)
+print(myarray)
