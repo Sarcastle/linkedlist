07cb3d78d45ef9aff209af7810bc7283a77f6f85
Felicity Graham <Felicity.rose.graham@outlook.com>
2023-12-22 07:31:11 -0800

    explaning work

--- insertionsort.py
+++ insertionsort.py
@@ -1,18 +1,17 @@
 def insertionSort(arr):
- 
-    for i in range(1, len(arr)):
- 
-        key = arr[i]
- 
-        
+    n = len(arr)  # gets length of array
+      
+    if n <= 1:
+        return  # if array has 0 or 1 it returns 
+    for i in range(1, n):  # iterates from array starting at second element 
+        key = arr[i] # stores the current element as the key 
         j = i-1
-        while j >= 0 and key < arr[j] :
-                arr[j + 1] = arr[j]
-                j -= 1
-        arr[j + 1] = key
- 
- 
-arr = [22, 4, 13, 7, 20]
+        while j >= 0 and key < arr[j]:  # elements greater than the key move one postion to the right
+            arr[j+1] = arr[j]  # shifts elements to the right
+            j -= 1
+        arr[j+1] = key  # places the key in the correct position
+  
+# sorts the array
+arr = [15, 4, 9, 20, 17]
 insertionSort(arr)
-for i in range(len(arr)):
-    print ("% d" % arr[i])
+print(arr)
